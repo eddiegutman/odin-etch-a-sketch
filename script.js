286df85ae418c7fc81afc45cdc4249e767757ff4
Eddie Gutman <edigutman@gmail.com>
2025-12-02 22:15:27 +0200

    Move grid creation into a function

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const SIZE = 16;
 const container = document.querySelector('#container');
 
-for (let i = 0; i < SIZE; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let j = 0; j < SIZE; j++) {
-    const col = document.createElement('div');
-    col.classList.add('col');
-    row.appendChild(col);
+function createGrid(size = 16) {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < size; j++) {
+      const col = document.createElement('div');
+      col.classList.add('col');
+      row.appendChild(col);
+    }
+    container?.appendChild(row);
   }
-  container?.appendChild(row);
+
+  document.addEventListener('mouseover', (e) => {
+    const target = e.target;
+    if (target.classList.contains('col')) {
+      target.classList.add('color');
+    }
+  });
 }
 
-document.addEventListener('mouseover', (e) => {
-  const target = e.target;
-  if (target.classList.contains('col')) {
-    target.classList.add('color');
-  }
-});
+createGrid();
