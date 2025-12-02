356ef3ac34b25a14aec1ffe2efdd571756067cc5
Eddie Gutman <edigutman@gmail.com>
2025-12-02 21:25:49 +0200

    Add basic grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const SIZE = 16;
+const container = document.querySelector('#container');
+
+for (let i = 0; i < SIZE; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < SIZE; j++) {
+    const col = document.createElement('div');
+    col.classList.add('col');
+    row.appendChild(col);
+  }
+  container?.appendChild(row);
+}
